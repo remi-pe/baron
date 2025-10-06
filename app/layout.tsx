15f3c21b74b7d9fd60e7aca3d1577430bd19765e
Remi Perrichon <perrichonremi@gmail.com>
2025-10-06 18:12:15 +0100

    feat: update meta description to 'Stay warm or die tryin'

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Coal Jack',
-  description: 'Stay warm or die.',
+  description: 'Stay warm or die tryin',
   generator: 'v0.app',
   icons: {
     icon: '/favicon.svg',
